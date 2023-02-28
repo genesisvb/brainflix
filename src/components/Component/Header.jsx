6c78e838b4b2a11ab8ae47e4df7e428bbc673c7d
Genesis Vivas <genesis.vivasb@gmail.com>
2023-02-27 23:43:31 -0800

    Initial commit

---
+++ src/components/Component/Header.jsx
@@ -0,0 +1,17 @@
+import logo from './assets/images/Logo/BrainFlix-logo.svg';
+
+function Header () {
+
+    <header className="App-header">
+        <div>
+            <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <div>
+            <input type="text" />
+            <div className="avatar"></div>
+        </div>
+        <button></button>
+    </header>
+}
+
+export default Header;
