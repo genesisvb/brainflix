3ee7643b9b6a7fd1367a904882b8dd482788c825
Genesis Vivas <genesis.vivasb@gmail.com>
2023-03-01 16:58:54 -0800

    Add Hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,12 @@
-// import React from 'react'
+import React from "react";
+import "./Hero.scss";
 
-// function Hero() {
-//   return (
-//     <div>Hero</div>
-//   )
-// }
+function Hero() {
+  return (
+    <div className="hero">
+        <video className="hero__video" src="https://project-2-api.herokuapp.com/stream" controls poster="https://i.imgur.com/l2Xfgpl.jpg"></video>
+    </div>
+  )
+}
 
-// export default Hero
+export default Hero;
