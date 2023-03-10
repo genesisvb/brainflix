9bd95f809b4c3a6dec16990edf42f0b3570a2e9a
Genesis Vivas <genesis.vivasb@gmail.com>
2023-03-09 18:40:25 -0800

    Add Video Upload Page and styling

--- src/components/Pages/VideoUpload/VideoUpload.jsx
+++ src/components/Pages/VideoUpload/VideoUpload.jsx
@@ -1,27 +1,32 @@
+import { Link } from "react-router-dom";
 import "./VideoUpload.scss";
-import Header from "../../Header/Header";
 
 function VideoUpload() {
   return (
     <>
-      <Header />
-      <main>
-        <h1>Upload Video</h1>
-        <p>Video Thumbnail</p>
-        <div>
+      <main className="video-upload">
+        <h2 className="video-upload__title">Upload Video</h2>
+        <p className="video-upload__subtitle">Video Thumbnail</p>
+        <div className="video__container">
           <video src=""></video>
         </div>
-        <div>
-          <label htmlFor="">Title your video</label>
-          <textarea name="videoTitle" id="">
+        <form className="input">
+          <label className="input__label" htmlFor="videoTitle">
+            Title your video
+          </label>
+          <textarea className="input__field" name="videoTitle" id="">
             Add a title to your video
           </textarea>
-          <label htmlFor="">Add a video description</label>
-          <textarea name="" id=""></textarea>
-        </div>
-        <div>
-          <button>Cancel</button>
-          <button>Publish</button>
+          <label className="input__label" htmlFor="videoDescription">
+            Add a video description
+          </label>
+          <textarea className="input__field" name="VideoDescription" id="">
+            Add a description to your video
+          </textarea>
+        </form>
+        <div className="buttons">
+          <Link className="button button--publish">Publish</Link>
+          <Link className="link">Cancel</Link>
         </div>
       </main>
     </>
