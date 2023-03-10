9fd63743f842a66bd53a2a096b595e610cdbfb75
Genesis Vivas <genesis.vivasb@gmail.com>
2023-03-09 18:42:37 -0800

    Update  Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,20 +5,28 @@ import inputIcon from "../../assets/Icons/search.svg";
 import HomePage from "../Pages/HomePage/HomePage";
 import { Link } from "react-router-dom";
 
-
-function Header () {
-    return (
-        <header className="header">
-            <nav className="nav">
-                {/* <Link to="/" </nav>><img className="nav__logo" src={logo}  alt="logo" /></Link> */}
-                <form className="nav__input">
-                    <input src={inputIcon} className="nav__input-box" type="text" placeholder="Search" />
-                    <img src={avatar} className="nav__input-photo" alt="avatar" />
-                    <button className="button">Upload</button>
-                </form>
-            </nav>
-        </header>
-    )       
-};
+function Header() {
+  return (
+    <header className="header">
+      <nav className="nav">
+        <Link to="/">
+          <img className="nav__logo" src={logo} alt="logo" />
+        </Link>
+        <form className="nav__input">
+          <input
+            src={inputIcon}
+            className="nav__input-box"
+            type="text"
+            placeholder="Search"
+          />
+          <img src={avatar} className="nav__input-photo" alt="avatar" />
+          <Link className="button" to="/upload">
+            Upload
+          </Link>
+        </form>
+      </nav>
+    </header>
+  );
+}
 
 export default Header;
