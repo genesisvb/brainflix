a212ef6aa3a6e9b0664e1ec636228396af55fee7
Genesis Vivas <genesis.vivasb@gmail.com>
2023-03-19 19:14:26 -0700

    Change link to button on upload video page as suggested on feedback

--- src/Pages/VideoUpload/VideoUpload.jsx
+++ src/Pages/VideoUpload/VideoUpload.jsx
@@ -1,8 +1,16 @@
 import { Link } from "react-router-dom";
 import "./VideoUpload.scss";
 import videoImage from "../../assets/images/Upload-video-preview.jpg";
+import { useNavigate } from "react-router-dom";
 
 function VideoUpload() {
+  const navigate = useNavigate();
+
+  function handleOnSubmit(e) {
+    e.preventDefault();
+    alert("Your video has been uploaded successfully!");
+    navigate("/");
+  }
   return (
     <>
       <main className="video-upload">
@@ -15,7 +23,7 @@ function VideoUpload() {
             alt="video cover  "
           />
           <div className="input-wrapper">
-            <form className="input">
+            <form className="input" onSubmit={handleOnSubmit}>
               <label className="input__label" htmlFor="videoTitle">
                 Title your video
               </label>
@@ -34,22 +42,16 @@ function VideoUpload() {
                 id=""
                 placeholder="Add a description to your video"
               ></textarea>
+              <div className="buttons">
+                <button type="submit" className="button button--publish">
+                  Publish
+                </button>
+                <Link to="/" className="link">
+                  Cancel
+                </Link>
+              </div>
             </form>
           </div>
-        </div>
-        <div className="buttons">
-          <Link
-            to="/"
-            className="button button--publish"
-            onClick={() => {
-              alert("Sorry, this feature is not available at this time");
-            }}
-          >
-            Publish
-          </Link>
-          <Link to="/" className="link">
-            Cancel
-          </Link>
         </div>
       </main>
     </>
@@ -57,3 +59,39 @@ function VideoUpload() {
 }
 
 export default VideoUpload;
+
+// /* ---------------------------
+//  * Post Jokes
+//  * To make a new joke we POST to https://developerjokes.herokuapp.com/jokes?api_key=neocat
+//  * How to make a post request? https://axios-http.com/docs/post_example
+//  */
+
+// jokeForm.addEventListener("submit", (e) => {
+//   e.preventDefault();
+
+//   const jokeInfo = {
+//     question: e.target.question.value,
+//     answer: e.target.answer.value,
+//   };
+
+//   axios
+//     .post(`${apiUrl}/jokes?api_key=${apiKey}`, jokeInfo)
+//     .then((response) => {
+//       console.log("response: ", response);
+
+//       const data = response.data;
+//       const updatedJokes = data.jokes;
+
+//       jokesList.innerHTML = "";
+//       updatedJokes.forEach((joke) => {
+//         renderJoke(joke.question, joke.answer, jokesList);
+//       });
+
+//       // after submission, we clear the form for better use experience!
+//       e.target.question.value = "";
+//       e.target.answer.value = "";
+//     })
+//     .catch((error) => {
+//       console.log("error: ", error);
+//     });
+// });
